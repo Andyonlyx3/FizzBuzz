b83800f19c5a9cb2ca8c531912c39a7e57f931cc
Olgeiser <olgeiser.andreas@training.lug-ag.de>
2024-11-27 16:02:41 +0100

    Spiel Fertig ohne Bin

--- obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
+++ obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+044aeffb5165f5a705afcac67c87ddbd09063ecb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+625f84197ceba448dbe7945c661f5dfcc84bc8d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
